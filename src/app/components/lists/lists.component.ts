cbd9f888f3d41500e7d09b3a72e0b685fed253c5
Jorge M. Alfaro Che <jorgm.alfarc@gmail.com>
2020-09-16 18:10:37 -0500

    tab 2 ruta terminada

--- src/app/components/lists/lists.component.ts
+++ src/app/components/lists/lists.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { WishesService } from '../../services/wishes.service';
 import { Tab1PageModule } from '../../pages/tab1/tab1.module';
 import { Tab2PageModule } from '../../pages/tab2/tab2.module';
@@ -12,6 +12,7 @@ import { Router } from '@angular/router';
 })
 export class ListsComponent implements OnInit {
 
+  @Input() terminada = true;
   constructor(public wishesService: WishesService,
               private router: Router) { }
 
@@ -19,7 +20,14 @@ export class ListsComponent implements OnInit {
 
 
   listaSelected( list: List ) {
-    this.router.navigateByUrl(`/tabs/tab1/agregar/${ list.id }`);
+
+    if ( this.terminada ){
+
+      this.router.navigateByUrl(`/tabs/tab2/agregar/${ list.id }`);
+    } else {
+
+      this.router.navigateByUrl(`/tabs/tab1/agregar/${ list.id }`);
+    }
   }
 
 }
